0ba956aad430d2ebb19c4dec9ee5e1e5695e18d6
ravidugm <ravindumiyuranga@gmail.com>
2021-03-31 14:08:07 +0530

    Change REST resource path as 'members'

--- src/main/java/com/practicecode/springboot/springbootcruddemo/dao/TeacherRepository.java
+++ src/main/java/com/practicecode/springboot/springbootcruddemo/dao/TeacherRepository.java
@@ -2,6 +2,8 @@ package com.practicecode.springboot.springbootcruddemo.dao;
 
 import com.practicecode.springboot.springbootcruddemo.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
 }
